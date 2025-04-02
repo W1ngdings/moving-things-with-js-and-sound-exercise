20249555f2bad0ef49a5fef04259b4a3129c2296
W1ngdings <emil35126@hotmail.com>
2025-04-02 13:30:09 +0200

    Dodger can't exit screen

--- index.js
+++ index.js
@@ -2,16 +2,20 @@
 "use strict"
 
 const dodger = document.getElementById("dodger");
-dodger.style.backgroundColor = "#ff69b4";
-dodger.style.bottom = "0px";
-dodger.style.left = "0px";
 
-document.addEventListener("keydown", function (event) {
-    if (event.key === "ArrowLeft") {
-        const leftNumbers = dodger.style.left.replace("px", "")
-        const left = parseInt(leftNumbers, 10);
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
 
-        dodger.style.left = `${left - 1}px`
-    } 
-});
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
 
