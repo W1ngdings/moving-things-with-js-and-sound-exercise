c29340fcff7b99b3bba4e0dce69685ce14d4cdb9
W1ngdings <emil35126@hotmail.com>
2025-04-02 13:50:04 +0200

    MoveDodgerRight

--- index.js
+++ index.js
@@ -15,7 +15,18 @@ function moveDodgerLeft() {
 
   document.addEventListener("keydown", function (e) {
     if (e.key === "ArrowLeft") {
-      moveDodgerLeft();
+        moveDodgerLeft();
     }
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+      }
   });
 
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`
+    }
+  }
