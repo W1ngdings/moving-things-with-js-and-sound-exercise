437b1168af885b30f324aade0165b58c9f1477f0
W1ngdings <emil35126@hotmail.com>
2025-04-02 13:20:52 +0200

    Move dodger left

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ dodger.style.bottom = "0px";
 dodger.style.left = "0px";
 
 document.addEventListener("keydown", function (event) {
-    console.log(event)
-}
-);
+    if (event.key === "ArrowLeft") {
+        const leftNumbers = dodger.style.left.replace("px", "")
+        const left = parseInt(leftNumbers, 10);
+
+        dodger.style.left = `${left - 1}px`
+    } 
+});
 
